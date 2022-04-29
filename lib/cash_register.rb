99effb6622b5c577420e525acf3ffc4ba3a2880a
Rahaf Alhabashi <rahafalhabashi@gmail.com>
2022-04-29 15:29:53 -0400

    commit tree e217874700fba09a99019b5128de35415f999c6c

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,33 @@
+class CashRegister
+    attr_accessor :total, :discount, :item, :last_transaction
 
+    def initialize(discount= 0)
+        @total = 0
+        @items = []
+        @discount = discount
+    end
+
+    def total(price, quantity) 
+        self.last_transaction = price * quantity
+        self.total += last_transaction
+    end
+    
+    def add_item (title, price, quantity)
+
+    end
+
+    def apply_discount
+
+    end
+
+    def items
+
+    end
+
+    def void_last_transaction(last_transaction)
+        total = total - last_transaction
+        if items = []
+            total = 0.0
+        end
+    end
+end
